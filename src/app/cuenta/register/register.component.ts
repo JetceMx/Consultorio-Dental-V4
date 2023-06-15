cc8e3610210447d1e349c726d214b6f34abb6cef
Luis Jimenez <luis.jimenez.cortes@outlook.com>
2023-06-15 16:26:18 -0600

    Validacion Contraseña
    Se le agrego la validacion de meter la contraseña dos veces y que sean iguales

--- src/app/cuenta/register/register.component.ts
+++ src/app/cuenta/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, AbstractControl, FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
 import { UsuariosService } from '../../usuarios.service';
 import { AlertifyService } from 'src/app/alertify.service';
@@ -12,16 +12,13 @@ import { AlertifyService } from 'src/app/alertify.service';
 export class RegisterComponent {
   formReg: FormGroup;
 
-  constructor(private alerta: AlertifyService,
-    private userService:UsuariosService,
-    private router: Router
-  ) {
-    this.formReg = new FormGroup({
+  constructor(private alerta: AlertifyService, private userService:UsuariosService, private router: Router, private formBuilder: FormBuilder) {
+    this.formReg = this.formBuilder.group({
       nombre: new FormControl('', [Validators.required, Validators.minLength(3)]),
       correo: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', [Validators.required, Validators.minLength(8)]),
       passwordVer: new FormControl('', [Validators.required,])
-    })
+    }, { validators: this.passwordMatchValidator })
   }
 
   ngOnInit(): void {
@@ -36,5 +33,15 @@ export class RegisterComponent {
       .catch(error => console.log(error));
   }
 
+  passwordMatchValidator(control: AbstractControl) {
+    const password = control?.get('password')?.value;
+    const confirmPassword = control?.get('passwordVer')?.value;
 
+    if (password !== confirmPassword) {
+      control?.get('passwordVer')?.setErrors({ passwordMismatch: true });
+      return { passwordMismatch: true };
+    } else {
+      return null;
+    }
+  }
 }
