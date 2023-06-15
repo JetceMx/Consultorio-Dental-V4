33edfc12640e60e6b546f505c40feee8f22fca2e
Oswaldo Cruz <kikke8508@gmail.com>
2023-06-15 15:58:03 -0600

    Formulario de registro semi actualizado

--- src/app/cuenta/register/register.component.ts
+++ src/app/cuenta/register/register.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
 import { UsuariosService } from '../../usuarios.service';
-import { Firestore, collection, addDoc, doc, setDoc } from '@angular/fire/firestore';
+import { Firestore, doc, setDoc } from '@angular/fire/firestore';
+import { formRegInterface } from '../formReg.model';
 
 @Component({
   selector: 'app-register',
@@ -10,6 +11,15 @@ import { Firestore, collection, addDoc, doc, setDoc } from '@angular/fire/firest
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+
+  formularioRegistro: formRegInterface = {
+    nombre: "",
+    apellido: "",
+    correo: "",
+    contra: "",
+    rol: "Paciente"
+  };
+
   formReg: FormGroup;
 
   constructor(
@@ -20,24 +30,30 @@ export class RegisterComponent {
     this.formReg = new FormGroup({
       email: new FormControl(),
       password: new FormControl(),
-      name: new FormControl()
+      name: new FormControl(),
+      lastname: new FormControl(),
     })
   }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
 
+  copiarDatos(): void {
+    this.formularioRegistro.nombre = this.formReg.value.name;
+    this.formularioRegistro.apellido = this.formReg.value.lastname;
+    this.formularioRegistro.correo = this.formReg.value.email;
+    this.formularioRegistro.contra = this.formReg.value.password;
+    console.log(this.formularioRegistro);
   }
 
   onSubmit() {
+    this.copiarDatos();
     this.userService.register(this.formReg.value)
       .then(async response => {
         console.log(response);
         const ruta = "Usuarios";
         let id1 = response.user.uid;
         console.log(response.user.uid)
-        await setDoc(doc(this.firestore, ruta, id1), this.formReg.value);
-
-
+        await setDoc(doc(this.firestore, ruta, id1), this.formularioRegistro);
         this.router.navigate(['Cuenta/loginE']);
       })
       .catch(error => console.log(error));
