69ebb28f67312bf79ff7d2f2940c679c39e9a310
Oswaldo Cruz <kikke8508@gmail.com>
2023-06-14 22:04:59 -0600

    Auth con Sebas

--- src/app/cuenta/register/register.component.ts
+++ src/app/cuenta/register/register.component.ts
@@ -2,9 +2,7 @@ import { Component } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
 import { UsuariosService } from '../../usuarios.service';
-import { Auth, updateCurrentUser, user } from '@angular/fire/auth';
-import { LocalCitasService } from 'src/app/citas/local-citas.service';
-import {Firestore, collection,addDoc, doc,setDoc } from '@angular/fire/firestore';
+import { Firestore, collection, addDoc, doc, setDoc } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-register',
@@ -15,9 +13,9 @@ export class RegisterComponent {
   formReg: FormGroup;
 
   constructor(
-    private userService:UsuariosService,
+    private userService: UsuariosService,
     private router: Router,
-    private firestore:Firestore
+    private firestore: Firestore
   ) {
     this.formReg = new FormGroup({
       email: new FormControl(),
@@ -39,7 +37,7 @@ export class RegisterComponent {
         console.log(response.user.uid)
         await setDoc(doc(this.firestore, ruta, id1), this.formReg.value);
 
-      
+
         this.router.navigate(['Cuenta/loginE']);
       })
       .catch(error => console.log(error));
